a4300f44b7b40cfad249c420b6d09b2e5a868208
Youngjae Lim <limyoungjae@gmail.com>
2022-01-07 23:56:35 -0500

    Landing Page - Basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Landing from './pages/Landing'
+
 function App() {
-  return <h1>Jobtrak</h1>
+  return (
+    <div>
+      <h1>Jobtrak</h1>
+      <Landing />
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,12 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+  return (
+    <div>
+      <h4>Landing Page</h4>
+    </div>
+  )
+}
+
+export default Landing
