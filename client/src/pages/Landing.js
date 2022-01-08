d8bf85328ea842d5e70e5fb89c9e13d601694b3e
Youngjae Lim <limyoungjae@gmail.com>
2022-01-08 00:13:51 -0500

    Landing Page - Basic structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,9 +3,27 @@ import main from '../assets/images/main.svg'
 
 const Landing = () => {
   return (
-    <div>
-      <h4>Landing Page</h4>
-    </div>
+    <main>
+      <nav>
+        <img src={logo} alt='jobtrak' className='logo' />
+      </nav>
+      <div class='container page'>
+        {/* info */}
+        <div class='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
+            autem laboriosam nostrum deserunt, earum quam quos fugiat est iure,
+            laudantium totam non quisquam consectetur quod id neque, dolor illo!
+            Magni.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
   )
 }
 
