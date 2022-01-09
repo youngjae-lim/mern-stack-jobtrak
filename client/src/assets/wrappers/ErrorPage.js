3afcf9e415676feefde123af40d8cedc7f71ed62
Youngjae Lim <limyoungjae@gmail.com>
2022-01-09 00:45:30 -0500

    Added contents to Error page

--- client/src/assets/wrappers/ErrorPage.js
+++ client/src/assets/wrappers/ErrorPage.js
@@ -2,14 +2,14 @@ import styled from 'styled-components'
 
 const Wrapper = styled.main`
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   img {
     max-width: 600px;
     display: block;
     margin-bottom: 2rem;
   }
-  display: flex;
-  align-items: center;
-  justify-content: center;
   h3 {
     margin-bottom: 0.5rem;
   }
