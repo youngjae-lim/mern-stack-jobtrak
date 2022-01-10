0125efb8ce08ae168f7d4efd797cc60f9e01972e
Youngjae Lim <limyoungjae@gmail.com>
2022-01-10 18:50:53 -0500

    Added a middleware for not found page

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Welcome!')
 })
+
+// middleware
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
