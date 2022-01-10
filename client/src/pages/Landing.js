56e40b1d1e798e313ad28c497181d4c462ea94a3
Youngjae Lim <limyoungjae@gmail.com>
2022-01-09 20:18:49 -0500

    Initial setup - register page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,6 +6,7 @@ const Landing = () => {
   return (
     <Wrapper>
       <nav>
+        {/* TODO: change log to Jobtrak */}
         <Logo />
       </nav>
       <div class='container page'>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,54 @@
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
+
 const Register = () => {
-  return <h1>Register</h1>
+  const [values, setValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        {/* TODO: change log to Jobtrak */}
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+
+          <input
+            type='text'
+            value={values.name}
+            name='name'
+            onChange={handleChange}
+            className='form-input'
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  )
 }
 
 export default Register
