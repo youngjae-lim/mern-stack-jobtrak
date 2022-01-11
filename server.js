adb391460edde56f41c8807acb9a872b4dbe2ce6
Youngjae Lim <limyoungjae@gmail.com>
2022-01-11 01:25:14 -0500

    Configured connection to mongodb atlas

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import dotenv from 'dotenv'
+import coonnectDB from './db/connect.js'
 
 dotenv.config()
 const app = express()
@@ -16,4 +17,15 @@ app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+const start = async () => {
+  try {
+    await coonnectDB(process.env.MONGO_URL)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
