d95e7c6fe71bebe14e20125f5cb7d3de272e7a03
Youngjae Lim <limyoungjae@gmail.com>
2022-01-12 23:42:04 -0500

    Removed password value in user object that is sent back to client in json format as a response to create a user

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,17 @@ const register = async (req, res) => {
   const user = await User.create({ name, email, password })
   const token = user.createJWT()
 
-  res.status(StatusCodes.CREATED).json({ user, token })
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      // hard-coded user object to exclude a hashed password manually
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  })
 }
 
 const login = async (req, res) => {
