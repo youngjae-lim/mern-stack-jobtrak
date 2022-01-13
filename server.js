43a6fbcf38dce7b1c7c5bd05a8f509b0d02b254a
Youngjae Lim <limyoungjae@gmail.com>
2022-01-13 14:44:04 -0500

    Replaced cors package with using proxy in development only

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import cors from 'cors'
 
 // middlewares
 import notFoundMiddleware from './middleware/not-found.js'
@@ -17,7 +16,6 @@ import jobsRouter from './routes/jobsRoutes.js'
 dotenv.config()
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
