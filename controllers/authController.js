ecb2661d2d4dae9ab7fa800c52106e0e972e3703
Youngjae Lim <limyoungjae@gmail.com>
2022-01-14 00:06:13 -0500

    Implemented login function in the authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js'
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
@@ -33,7 +33,29 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  return res.send('login')
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('please provide all values')
+  }
+
+  // find a user by email
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user) {
+    throw new UnauthenticatedError('invalid credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('invalid credentials')
+  }
+
+  const token = user.createJWT()
+  // set password to undefined so that we don't expose the hashed password
+  user.password = undefined
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 const updateUser = async (req, res) => {
