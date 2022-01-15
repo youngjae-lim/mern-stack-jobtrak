42e47892ac67fa6d734ffa0617a06ce40d847224
Youngjae Lim <limyoungjae@gmail.com>
2022-01-15 11:30:46 -0500

    Added toggleSidebar to SmallSidebar component

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,14 +1,21 @@
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { useAppContext } from '../context/appContext'
 import { FaTimes } from 'react-icons/fa'
 import links from '../utils/links'
 import Logo from './Logo'
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext()
+
   return (
     <Wrapper>
-      <div className='sidebar-container show-sidebar'>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
         <div className='content'>
-          <button className='close-btn' onClick={() => console.log('toggle')}>
+          <button className='close-btn' onClick={toggleSidebar}>
             <FaTimes />
           </button>
           <header>
