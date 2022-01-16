2cb752603c5c10fd7b81abf61a94f5708faf5524
Youngjae Lim <limyoungjae@gmail.com>
2022-01-15 21:47:52 -0500

    Added UnauthenticatedError to auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,11 @@
+import { UnauthenticatedError } from '../errors/index.js'
+
 const auth = async (req, res, next) => {
-  const headers = req.headers
   const authHeader = req.headers.authorization
-  console.log(headers)
-  console.log(authHeader)
+
+  if (!authHeader) {
+    throw new UnauthenticatedError('Authentication Invalid')
+  }
   next()
 }
 
