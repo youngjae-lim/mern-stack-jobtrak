60cf9c20925b5c574911f68e43bd902d84554586
Youngjae Lim <limyoungjae@gmail.com>
2022-01-16 20:45:52 -0500

    Configured axios interceptors to control a different type of error message as well as passing authorization header

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -34,12 +34,35 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  // axios config
   const authFetch = axios.create({
     baseURL: '/api/v1',
-    headers: {
-      Authorization: `Bearer ${state.token}`,
-    },
   })
+
+  // request interceptor
+  authFetch.interceptors.request.use(
+    (config) => {
+      // config.headers.common['Authorization'] = `Bearer ${state.token}`
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+
+  // response interceptor
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response
+    },
+    (error) => {
+      console.log(error.response)
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR')
+      }
+      return Promise.reject(error)
+    }
+  )
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT })
@@ -105,7 +128,7 @@ const AppProvider = ({ children }) => {
       const { data } = await authFetch.patch('/auth/updateUser', currentUser)
       console.log(data)
     } catch (error) {
-      console.log(error.response)
+      // console.log(error.response)
     }
   }
   // const handleChange = ({ name, value }) => {
