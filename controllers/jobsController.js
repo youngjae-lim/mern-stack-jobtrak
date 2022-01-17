b66601584706c820da39bf44be068fd5761a5759
Youngjae Lim <limyoungjae@gmail.com>
2022-01-17 00:11:04 -0500

    Completed createJob() in jobsController.js

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,18 @@
+import Job from '../models/Job.js'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError, NotFoundError } from '../errors/index.js'
+
 const createJob = async (req, res) => {
-  return res.send('createJob')
+  const { position, company } = req.body
+
+  if (!position || !company) {
+    throw new BadRequestError('please provide all values')
+  }
+
+  req.body.createdBy = req.user.userId
+
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const deleteJob = async (req, res) => {
