dffe0880bcc4092424de1419357aec5fcec35ce2
Youngjae Lim <limyoungjae@gmail.com>
2022-01-17 23:24:54 -0500

    Completed setEditJob functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -18,6 +18,7 @@ import {
   CREATE_JOB_ERROR,
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from './actions'
 import axios from 'axios'
 
@@ -238,7 +239,11 @@ const AppProvider = ({ children }) => {
   }
 
   const setEditJob = (id) => {
-    console.log(`set edit job: ${id}`)
+    dispatch({ type: SET_EDIT_JOB, payload: { id } })
+  }
+
+  const editJob = () => {
+    console.log('edit job')
   }
 
   const deleteJob = (id) => {
@@ -259,6 +264,7 @@ const AppProvider = ({ children }) => {
         createJob,
         getJobs,
         setEditJob,
+        editJob,
         deleteJob,
       }}
     >
