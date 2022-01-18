ac7a70f76ec8fe7b0a0d52ea69344cc692dc291b
Youngjae Lim <limyoungjae@gmail.com>
2022-01-17 21:36:06 -0500

    Added more elements to Job component such as position, company, edit, and delete buttons

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -237,6 +237,14 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const setEditJob = (id) => {
+    console.log(`set edit job: ${id}`)
+  }
+
+  const deleteJob = (id) => {
+    console.log(`delete: ${id}`)
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -250,6 +258,8 @@ const AppProvider = ({ children }) => {
         clearValues,
         createJob,
         getJobs,
+        setEditJob,
+        deleteJob,
       }}
     >
       {children}
