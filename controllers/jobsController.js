03e1b0209cde6d14cd083c4020b537cc56874858
Youngjae Lim <limyoungjae@gmail.com>
2022-01-19 11:06:23 -0500

    Added a group by query to have monthlyApplications in showStats()

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -85,7 +85,24 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   }
 
-  let monthlyApplications = []
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          year: {
+            $year: '$createdAt',
+          },
+          month: {
+            $month: '$createdAt',
+          },
+        },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } }, // decreasing order
+    { $limit: 6 }, // the latest 6 months
+  ])
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
