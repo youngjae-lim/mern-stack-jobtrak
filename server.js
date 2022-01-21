7d69250634e16fa43c595155d1b9e1f89b8265ed
Youngjae Lim <limyoungjae@gmail.com>
2022-01-21 17:26:34 -0500

    Added security packages and updated server.js accordingly and update .svg for logo

--- server.js
+++ server.js
@@ -14,20 +14,39 @@ import connectDB from './db/connect.js'
 import authRouter from './routes/authRoutes.js'
 import jobsRouter from './routes/jobsRoutes.js'
 
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+// packages for security
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
 dotenv.config()
 const app = express()
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
-app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Welcome!')
-})
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// only when ready to deploy -- the location of static assests in the production
+// app.use(express.static(path.resolve(__dirname, './client/build')))
+
+app.use(express.json())
+app.use(helmet()) // secure express apps by setting various HTTP headers
+app.use(xss()) // sanitize user input coming from POST body, GET queries, and url params
+app.use(mongoSanitize()) // sanitize user-supplied data to prevent MongoDB operator injection
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+// only when ready to deploy
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+// })
 
 // middleware
 app.use(notFoundMiddleware)
